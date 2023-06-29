8fab63c92ae6e35ac2c3010bb896a35b6546de1c
chetan <chetan@CHAITANYA>
2023-06-29 15:19:56 +0530

    i commited

--- src/main/java/ObjectRepository/Organizationlookup.java
+++ src/main/java/ObjectRepository/Organizationlookup.java
@@ -1,5 +1,20 @@
 package ObjectRepository;
 
-public class Organizationlookup {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Organizationlookup { 
+	@FindBy(xpath = "//img[@title=\"Create Organization...\"]")private WebElement orgLookuplink;
+	//constructor
+	public Organizationlookup(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	public WebElement getOrgLookuplink() {
+		return orgLookuplink;
+	}
+	
 
 }
--- src/main/java/ObjectRepository/homePage.java
+++ src/main/java/ObjectRepository/homePage.java
@@ -3,6 +3,7 @@ package ObjectRepository;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class homePage {
 
@@ -14,10 +15,39 @@ public class homePage {
 	@FindBy(xpath = "//a[text()='Contacts']")private WebElement  contactLink;
 	@FindBy(xpath = "//a[text()='Opportunities']")private WebElement opportunityLink;
 	@FindBy(xpath = "(//td[@class='small'])[2]")private WebElement adminimage;
-	@FindBy(xpath = "")private WebElement signout;
+	@FindBy(xpath = "(//a[@class='drop_down_usersettings'])[2]")private WebElement signout;
 	
 //	constructor
 	
-	public WebDriver driver;
+	public homePage (WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public WebElement getOrgLink() {
+		return orgLink;
+	}
+
+	public WebElement getContactLink() {
+		return contactLink;
+	}
+
+	public WebElement getOpportunityLink() {
+		return opportunityLink;
+	}
+
+	public WebElement getAdminimage() {
+		return adminimage;
+	}
+
+	public WebElement getSignout() {
+		return signout;
+	}
+	
 }
 
